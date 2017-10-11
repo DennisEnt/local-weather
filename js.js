a0e1b365f97c300fa7ff87d30ee9b45ad979957d
Dennis Ent <dennis.p.ent@gmail.com>
2017-10-11 16:02:47 +1300

    Add files via upload

--- js.js
+++ js.js
@@ -1,52 +1,69 @@
 window.onload = () => {
-
-  navigator.geolocation.getCurrentPosition(function (position) {
-    // console.log(position);
-    fetch(`https://api.wunderground.com/api/36708126e28ee6ce/conditions/q/${position.coords.latitude},${position.coords.longitude}.json'`, {
-      method: 'get' })
-      .then(res=>res.json())
-      .then(function (data) {
-      const tempC = data.current_observation.temp_c;
-      const tempF = data.current_observation.temp_f;
-      const weather = data.current_observation.weather;
-      const city = data.current_observation.display_location.full;
-      const icon = data.current_observation.icon;
-      console.log(data);
-      document.getElementById("city").innerHTML = `${city}`;
-      document.getElementById("temp").innerHTML = `${tempC}&#8451`;
-      document.getElementById("weather").innerHTML = weather;
-      var skycons = new Skycons({
-        "color": "black"
+  navigator.geolocation.getCurrentPosition(position => {
+    let { latitude, longitude } = position.coords;
+    fetch(
+      `https://api.wunderground.com/api/36708126e28ee6ce/conditions/q/${latitude},${longitude}.json'`,
+      {
+        method: 'get'
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        const tempC = data.current_observation.temp_c;
+        const formatTemp = tempC.toFixed(0);
+        const weather = data.current_observation.weather;
+        const city = data.current_observation.display_location.full;
+        const icon = data.current_observation.icon;
+        console.log(data.current_observation.icon);
+        document.getElementById('city').innerHTML = `${city}`;
+        document.getElementById('temp').innerHTML = `${formatTemp}&#8451`;
+        document.getElementById('weather').innerHTML = weather;
+        var skycons = new Skycons({
+          color: 'black'
+        });
+        switch (icon) {
+          case 'fog':
+          case 'hazy':
+            skycons.add('icon1', Skycons.FOG);
+            skycons.play();
+            break;
+          case 'sleet':
+          case 'chancesleet':
+            skycons.add('icon1', Skycons.SLEET);
+            skycons.play();
+            break;
+          case 'chancerain':
+          case 'chancetstorms':
+          case 'rain':
+          case 'tstorms':
+          case 'unknown':
+            skycons.add('icon1', Skycons.RAIN);
+            skycons.play();
+            break;
+          case 'chancetsnow':
+          case 'flurries':
+          case 'chanceflurries':
+          case 'snow':
+            skycons.add('icon1', Skycons.SNOW);
+            skycons.play();
+            break;
+          case 'mostlycloudy':
+          case 'partlycloudy':
+          case 'partlysunny':
+            skycons.add('icon1', Skycons.PARTLY_CLOUDY_DAY);
+            skycons.play();
+            break;
+          case 'sunny':
+          case 'mostlysunny':
+          case 'clear':
+            skycons.add('icon1', Skycons.CLEAR_DAY);
+            skycons.play();
+            break;
+          case 'cloudy':
+            skycons.add('icon1', Skycons.CLOUDY);
+            skycons.play();
+            break;
+        }
       });
-      if (icon == "fog" || "hazy") {
-        skycons.add("icon1", Skycons.FOG);
-        skycons.play();
-      }
-      else if (icon == "cloudy") {
-        skycons.add("icon1", Skycons.CLOUDY);
-        skycons.play();
-      }
-      else if (icon == "chancerain" || "chancetstorms" || "rain" || "tstorms" || "unknown") {
-        skycons.add("icon1", Skycons.RAIN);
-        skycons.play();
-      }
-      else if (icon == "chancesleet" || "sleet") {
-        skycons.add("icon1", Skycons.SLEET);
-        skycons.play();
-      }
-      else if (icon == "chancesnow" || "flurries" || "chanceflurries" || "snow") {
-        skycons.add("icon1", Skycons.SNOW);
-        skycons.play();
-      }
-      else if (icon == "mostlycloudy" || "partlycloudy") {
-        skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
-        skycons.play();
-      }
-      else if (icon == "clear" || "mostlysunny" || "partlysunny" || "sunny") {
-        skycons.add("icon1", Skycons.CLEAR_DAY);
-        skycons.play();
-      }
-
-    });
-  })
-}
+  });
+};
