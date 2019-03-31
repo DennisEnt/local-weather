a0e8beb80c9c5fbc9ab0a24601861aec8732064c
Dennis <dennisent@protonmail.com>
2019-03-31 15:09:26 +1100

    Add files via upload

--- js.js
+++ js.js
@@ -10,64 +10,69 @@ navigator.geolocation.getCurrentPosition(position => {
   )
     .then(res => res.json())
     .then(data => {
-      console.log(data.weather[0].description);
       const tempC = data.main.temp;
       const formatTemp = tempC.toFixed(0);
-      const weather = data.weather[0].main;
+      const weather = data.weather[0].description;
       const city = data.name;
-      const icon = data.weather[0].description;
+      const icon = data.weather[0].icon;
       document.getElementById('city').innerHTML = `${city}`;
       document.getElementById('temperature').innerHTML = `Today's Temperature:`;
       document.getElementById('temp').innerHTML = `${formatTemp}&#8451`;
-      // document.getElementById(
-      //   'forecast'
-      // ).innerHTML = `Today's Weather Forecast:`;
-      // document.getElementById('weather').innerHTML = weather;
-      // var skycons = new Skycons({
-      //   color: 'black'
-      // });
-      // switch (icon) {
-      //   case 'fog':
-      //   case 'hazy':
-      //     skycons.add('icon1', Skycons.FOG);
-      //     skycons.play();
-      //     break;
-      //   case 'sleet':
-      //   case 'chancesleet':
-      //     skycons.add('icon1', Skycons.SLEET);
-      //     skycons.play();
-      //     break;
-      //   case 'chancerain':
-      //   case 'chancetstorms':
-      //   case 'rain':
-      //   case 'tstorms':
-      //   case 'unknown':
-      //     skycons.add('icon1', Skycons.RAIN);
-      //     skycons.play();
-      //     break;
-      //   case 'chancetsnow':
-      //   case 'flurries':
-      //   case 'chanceflurries':
-      //   case 'snow':
-      //     skycons.add('icon1', Skycons.SNOW);
-      //     skycons.play();
-      //     break;
-      //   case 'mostlycloudy':
-      //   case 'partlycloudy':
-      //   case 'partlysunny':
-      //     skycons.add('icon1', Skycons.PARTLY_CLOUDY_DAY);
-      //     skycons.play();
-      //     break;
-      //   case 'sunny':
-      //   case 'mostlysunny':
-      //   case 'clear':
-      //     skycons.add('icon1', Skycons.CLEAR_DAY);
-      //     skycons.play();
-      //     break;
-      //   case 'cloudy':
-      //     skycons.add('icon1', Skycons.CLOUDY);
-      //     skycons.play();
-      //     break;
-      // }
+      document.getElementById(
+        'forecast'
+      ).innerHTML = `Today's Weather Forecast:`;
+      document.getElementById('weather').innerHTML = weather;
+      var skycons = new Skycons({
+        color: 'black'
+      });
+      switch (icon) {
+        case '01d':
+          skycons.add('icon1', Skycons.CLEAR_DAY);
+          skycons.play();
+          break;
+
+        case '01n':
+          skycons.add('icon1', Skycons.CLEAR_NIGHT);
+          skycons.play();
+          break;
+
+        case '02d':
+          skycons.add('icon1', Skycons.PARTLY_CLOUDY_DAY);
+          skycons.play();
+          break;
+
+        case '02n':
+          skycons.add('icon1', Skycons.PARTLY_CLOUDY_NIGHT);
+          skycons.play();
+          break;
+        case '03d':
+        case '03n':
+        case '04d':
+        case '04n':
+          skycons.add('icon1', Skycons.CLOUDY);
+          skycons.play();
+          break;
+        case '09d':
+        case '09n':
+          skycons.add('icon1', Skycons.RAIN);
+          skycons.play();
+          break;
+        case '10d':
+        case '10n':
+        case '11d':
+        case '11n':
+          skycons.add('icon1', Skycons.SLEET);
+          skycons.play();
+          break;
+        case '13d':
+        case '13n':
+          skycons.add('icon1', Skycons.SNOW);
+          skycons.play();
+          break;
+        default:
+          skycons.add('icon1', Skycons.FOG);
+          skycons.play();
+          break;
+      }
     });
 });
